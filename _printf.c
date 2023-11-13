81a39424c29396b7d9f14de329a8e7cf58d5100b
OtaOkpogo <Otaokpogo@yahoo.com>
2023-11-12 22:22:08 -0800

    created _printf.c

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-int u, printed = 0, printed_chars = 0;
+int i, printed = 0, printed_chars = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
 char buffer[BUFF_SIZE];
@@ -19,11 +19,11 @@ return (-1);
 
 va_start(list, format);
 
-for (u = 0; format && format[u] != '\0'; u++)
+for (i = 0; format && format[i] != '\0'; i++)
 {
-if (format[u] != '%')
+if (format[i] != '%')
 {
-buffer[buff_ind++] = format[u];
+buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
 print_buffer(buffer, &buff_ind);
 /* write(1, &format[i], 1);*/
@@ -32,12 +32,12 @@ printed_chars++;
 else
 {
 print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &u);
-width = get_width(format, &u, list);
-precision = get_precision(format, &u, list);
-size = get_size(format, &u);
-++u;
-printed = handle_print(format, &u, list, buffer,
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
 flags, width, precision, size);
 if (printed == -1)
 return (-1);
