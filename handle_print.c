c80348c8ce4325a70957bdfeefab56ce91a256c7
Stanley Obianke <141111381+Stancodez@users.noreply.github.com>
2023-11-14 06:21:05 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * handle_print - It prints an argument based on its type
  * @fmt: Formatted string to print the arguments.
@@ -11,6 +12,7 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
+
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
